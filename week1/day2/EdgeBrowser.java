d441a6ee83d557b5192816fa9dce71b1534219c6
badma2903 <139243863+badma2903@users.noreply.github.com>
2024-11-13 14:19:10 +0530

    Update EdgeBrowser.java

--- week1/day2/EdgeBrowser.java
+++ week1/day2/EdgeBrowser.java
@@ -2,11 +2,9 @@ package week1.day2;
 
 public class EdgeBrowser 
 {
-
-	public static void main(String[] args)
+   public static void main(String[] args)
 	{
 		Browser bo = new Browser();
-		
 		
 		String launchBrowser = bo.launchBrowser("Microsoft Edge");
 		System.out.println(launchBrowser);
