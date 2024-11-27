f1297e389584e2570a148a32c995fb6f51d9055b
badma2903 <139243863+badma2903@users.noreply.github.com>
2024-11-27 08:13:46 +0530

    Update WebElement.java

--- week3/day1/WebElement.java
+++ week3/day1/WebElement.java
@@ -1,15 +1,20 @@
 package assignments.week3.day1;
 
+//Creating a class
 public class WebElement 
 {
+	//Initializing the method & pass the arguments
 	public void  click(String msg)
 	{
+		//Prints the message
 		System.out.println("Click:"+msg);
 	
 	}
 	
+	//Initializing the method & pass the arguments
 	public void setText(String text)
 	{
+		//Prints the message
 		System.out.println("Set Text as:"+text);
 		
 	}
