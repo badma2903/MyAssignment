731eb17f3cb62f443fb93d75de64be1c6de73f21
badma2903 <139243863+badma2903@users.noreply.github.com>
2024-11-27 07:51:30 +0530

    Update APIClient.java

--- week3/day1/APIClient.java
+++ week3/day1/APIClient.java
@@ -2,14 +2,18 @@ package assignments.week3.day1;
 
 public class APIClient 
 {
+	//Using sendRequest method by passing string args
 	public void sendRequest(String endpoint)
 	{
 		System.out.println("Send the Request as:"+endpoint);
 	}
+	
+	//Using sendRequest method by passing string args and boolean value
 	public void sendRequest(String endpoint, String requestBody, Boolean requestStatus )
 	{
 		System.out.println("Send the Request as:"+endpoint+"\n"+"Name the Driver as:"+requestBody+"\n"+"Status is :"+requestStatus);
 	}
+	//Method overloading
 	public static void main(String[] args) 
 	{
 		APIClient app = new APIClient();
