04be32af27f083211ff0a709c8ae9597acf0f958
badma2903 <139243863+badma2903@users.noreply.github.com>
2024-11-27 08:12:21 +0530

    Update TextField.java

--- week3/day1/TextField.java
+++ week3/day1/TextField.java
@@ -1,15 +1,19 @@
 package assignments.week3.day1;
 
+//Inherits the Web element class using extend keyword
 public class TextField extends WebElement
 {
+	//Creating the void method 
 	public void  getText()
 	{
+		// Using super keyword
 		super.click("Enter Button");
 	
 	}
 	
 	public static void main(String[] args) 
 	{
+		//Creating object for the classs
 		TextField tf = new TextField();
 		tf.click("Next");
 		tf.setText("Successful");
