869fa0ab4e4af10bb6bf5f5e898cf1db741a08fc
badma2903 <139243863+badma2903@users.noreply.github.com>
2024-11-27 07:56:28 +0530

    Update CheckBoxButton.java

--- week3/day1/CheckBoxButton.java
+++ week3/day1/CheckBoxButton.java
@@ -1,16 +1,18 @@
 package assignments.week3.day1;
-
+ //Inherits the Button class using extends
 public class CheckBoxButton extends Button
 {
-
+	//Creating new method
 	public void clickCheckButton()
 	{
+		//prints the message
 		System.out.println("Verifying it clicks the button");
 	
 	}
 	
 	public static void main(String[] args)
 	{
+		//Create the object for the class
 		CheckBoxButton cb = new CheckBoxButton();
 		cb.click("Login");
 		cb.setText("Successful");
