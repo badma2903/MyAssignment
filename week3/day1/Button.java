c9f9eb75f4a332a3df218cc5ee21e82a845af1c8
badma2903 <139243863+badma2903@users.noreply.github.com>
2024-11-27 07:54:47 +0530

    Update Button.java

--- week3/day1/Button.java
+++ week3/day1/Button.java
@@ -1,15 +1,19 @@
 package assignments.week3.day1;
 
+//Inherits the WebElement class using extends 
 public class Button extends WebElement
 {
+	// creating method
 	public void  Submit()
 	{
+		//Print the msg
 		System.out.println("Submit the Button");
 		
 	
 	}
 	public static void main(String[] args) 
 	{
+		//creating object for the cls 
 		Button bt = new Button();
 		bt.click("ok");
 		bt.setText("Successful");
